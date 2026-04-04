8f57f6fccb1e68e9f59e718a5ce7da71a67184f1
KoSoeMin <kosoemin2244@gmial.com>
2026-04-04 16:34:59 +0630

    initial-commit including by authentication

---
+++ app/Http/Controllers/Product/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Product\ProductController;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+}
